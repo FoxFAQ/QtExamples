2cdd5902e8295b6daf834f2ffa9bf9b2e0ab84e7
Phoenix_Shepard <Illchangethisworld@gmail.com>
2019-04-19 16:06:11 +0300

    QTreeWidget QTreeWIdgetItem 21

---
+++ QTreeWidget/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
+#include <QDebug>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_treeWidget_itemClicked(QTreeWidgetItem *item, int column);
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
